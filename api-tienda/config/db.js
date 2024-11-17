16eec69ffa3d4b273ca7810c2b0bb537239af06e
JafetOrellana <orellanajafet507@gmail.com>
2024-11-16 19:43:37 -0600

    Mejorando el control de errores en la conexion a la DB

--- api-tienda/config/db.js
+++ api-tienda/config/db.js
@@ -2,15 +2,21 @@ import mysql from 'mysql';
 import 'dotenv/config';
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+     host: process.env.DB_HOST,
+     port: process.env.DB_PORT,
+     user: process.env.DB_USER,
+     password: process.env.DB_PASSWORD,
+     database: process.env.DB_NAME,
+ });
 
 connection.connect((error) => {
-    throw new Error('El error de conexion es: ', error);
-})
+    if (error) {
+        console.error('Error de conexión:', error.message);
+        process.exit(1);
+    } else {
+        console.log('Conexión exitosa a la base de datos');
+    }
+});
+
 
 export default connection;
